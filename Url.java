0a6e148fadeb469b7f4ca97ab908c106b4f19df0
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-04-04 19:35:34 +0200

    Delete Url.java

--- Url.java
+++
@@ -1,8 +0,0 @@
-package etu1932.framework;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface Url {
-    public String key() default "";
-}
