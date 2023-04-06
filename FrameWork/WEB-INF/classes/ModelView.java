417338ab5e18d92007b32ac0a4974d658965043f
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-04-06 22:47:06 +0200

    Add files via upload

--- FrameWork/WEB-INF/classes/ModelView.java
+++ FrameWork/WEB-INF/classes/ModelView.java
@@ -1,6 +1,14 @@
 package etu1932.framework;
+
+import java.util.HashMap;
+
 public class ModelView {
     String Url;
+    HashMap<String, Object> data = new HashMap<>();
+
+    public ModelView(){
+        
+    }
 
     public String getUrl() {
         return Url;
@@ -8,6 +16,18 @@ public class ModelView {
 
     public void setUrl(String url) {
         Url = url;
+    }
+
+    public HashMap<String, Object> getData(){
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data){
+        this.data = data;
+    }
+
+    public void AddItem(String key, Object Value){
+        data.put(key, Value);
     }
 
     public ModelView(String url){
